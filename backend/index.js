1469bcc39da07af28e2c02d2d16d93a5c82940a1
Tomislav Dukez <tomdu3@ymail.com>
2024-07-05 12:45:28 +0200

    setup and connect with db

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+
+
+if (!process.env.DATABASE_URL) {
+    throw new Error('DATABASE_URL environment variable is not set');
+}
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+});
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
+
+module.exports = sequelize
