2f48fbabfc167cdb82a01e5a69249ef082e2b6ea
Tomislav Dukez <tomdu3@ymail.com>
2024-07-06 09:03:34 +0200

    add models and default settings

--- backend/index.js
+++ backend/index.js
@@ -11,13 +11,49 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
 });
 
-sequelize
-    .authenticate()
-    .then(() => {
-        console.log('Connection has been established successfully.');
-    })
-    .catch(err => {
-        console.error('Unable to connect to the database:', err);
-    });
+// sequelize
+//     .authenticate()
+//     .then(() => {
+//         console.log('Connection has been established successfully.');
+//     })
+//     .catch(err => {
+//         console.error('Unable to connect to the database:', err);
+//     });
 
-module.exports = sequelize
+const User = sequelize.define('user', {
+    user_id: {
+        type: Sequelize.DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    username: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+        
+
+
+    },
+    password: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+    },
+    is_admin : {
+        type: Sequelize.DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+    }
+},
+{
+    freezeTableName: true,
+    timestamps: false,
+});
+
+User.sync({alter: true}).then(() =>{
+    console.log('Table and model synced successfully');
+}).catch((err) => {
+    console.error('Unable to sync table and model:', err);
+});
