6528aefb96d893eab2ab447e069d55a8fb06962e
Tomislav Dukez <tomdu3@ymail.com>
2024-07-07 10:38:47 +0200

    add finder/raw argument

---
+++ backend/finder.js
@@ -0,0 +1,62 @@
+const Sequelize = require('sequelize');
+const { defaultValueSchemable } = require('sequelize/lib/utils');
+const { DataTypes } = Sequelize;
+require('dotenv').config();
+
+
+if (!process.env.DATABASE_URL) {
+    throw new Error('DATABASE_URL environment variable is not set');
+}
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    define : {
+        freezeTableName: true // do not pluralize table names for every model 
+    }
+});
+
+const Student = sequelize.define('student', {
+    student_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validators: {
+            notEmpty: true,
+            length: {
+                min: 4,
+                max: 20
+            }
+        }
+    },
+    favorite_class: {
+        type: DataTypes.STRING,
+        defaultValue: 'Computer Science',
+    },
+    school_year: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    subscribed_to_wittcode: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+    }
+});
+
+Student.sync().then(() =>{
+
+   // finder method instead of findAll
+   return Student.findAll({
+    where: { subscribed_to_wittcode: true },
+    raw: true,  // it is like toJSON()
+    })
+})
+.then((data) => {
+    console.log(data)
+})
+.catch((err) => {
+    console.error('Unable to sync table and model:', err);
+});
